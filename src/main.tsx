54b8d67a36a5f05a9006d35b417d9b5ee799f41d
sgtlaugh <sgtlaugh@gmail.com>
2026-05-16 00:00:22 +0600

    perf: remove React StrictMode to avoid duplicate API calls

--- src/main.tsx
+++ src/main.tsx
@@ -4,8 +4,4 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
