dbda29743bf9a5ba6b06e31541ecdcde98e8c232
sgtlaugh <sgtlaugh@gmail.com>
2026-06-09 17:54:03 +0600

    feat: add blue/black dev-home logo as favicon and electron icon

--- electron/main.ts
+++ electron/main.ts
@@ -51,6 +51,7 @@ function createWindow() {
     height: 860,
     minWidth: 900,
     minHeight: 600,
+    icon: path.join(__dirname, "../public/favicon.png"),
     titleBarStyle: "hiddenInset",
     backgroundColor: "#0a0a0a",
     webPreferences: {
